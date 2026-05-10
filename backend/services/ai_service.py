319aff15a0af9ab692c2819650e72ac73341c4ac
psa21git <psa.dev.21@gmail.com>
2026-05-10 22:43:30 +0530

    smALLER SUMM MODEL

--- backend/services/ai_service.py
+++ backend/services/ai_service.py
@@ -9,7 +9,7 @@ class SummarizerService:
     def load_model(self):
         try:
             print("Loading summarization model...")
-            self.summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+            self.summarizer = pipeline("summarization", model="Falconsai/text_summarization")
             print("Model loaded successfully.")
         except Exception as e:
             print(f"Error loading model: {e}")
